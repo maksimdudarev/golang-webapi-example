b4723d5797e0f578ca71d4ced2d912e7c4c2531b
Maksim Dudarev <dudarev_mn@a-med.space>
2024-08-23 18:24:09 +0300

    add pg db and gorm

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ WORKDIR /usr/src/app
 
 RUN go mod init github.com/maksimdudarev/golang-webapi-example
 RUN go get github.com/gofiber/fiber/v2
+RUN go get gorm.io/gorm
+RUN go get gorm.io/driver/postgres
 
 COPY . .
 RUN go mod tidy
--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/maksimdudarev/golang-webapi-example/database"
+)
 
 func main() {
+	database.ConnectDb()
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
