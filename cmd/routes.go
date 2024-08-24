56c01a153a5ece91cd7310965e671f32d219af4a
Maksim Dudarev <dudarev_mn@a-med.space>
2024-08-24 14:29:04 +0300

    add routes

---
+++ cmd/routes.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/maksimdudarev/golang-webapi-example/handlers"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/", handlers.Home)
+}
