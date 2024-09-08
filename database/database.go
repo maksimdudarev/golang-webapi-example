d543603da449c4505916ad48e262cdf36642524f
Maksim Dudarev <dudarev_mn@a-med.space>
2024-09-08 21:02:11 +0300

    add DB_HOST & container names

--- database/database.go
+++ database/database.go
@@ -18,8 +18,8 @@ type Dbinstance struct {
 var DB Dbinstance
 
 func ConnectDb() {
-	dsn := fmt.Sprintf("host=db user=%s password=%s dbname=%s port=5432",
-		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432",
+		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
