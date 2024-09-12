df51543e3aa35c5f4ce7b215c2088a5d0a2dca9d
Maksim Dudarev <dudarev_mn@a-med.space>
2024-09-13 01:12:49 +0300

    main & routes move

--- Example1/main.go
+++ Example1/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/maksimdudarev/golang-webapi-example/database"
+	"github.com/maksimdudarev/golang-webapi-example/routes"
 )
 
 func main() {
 	database.ConnectDb()
 	app := fiber.New()
 
-	setupRoutes(app)
+	routes.SetupRoutes(app)
 
 	app.Listen(":3000")
 }
--- Example1/routes/routes.go
+++ Example1/routes/routes.go
@@ -1,11 +1,11 @@
-package main
+package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/maksimdudarev/golang-webapi-example/handlers"
 )
 
-func setupRoutes(app *fiber.App) {
+func SetupRoutes(app *fiber.App) {
 	app.Get("/", handlers.ListFacts)
 
 	app.Post("/fact", handlers.CreateFact)
