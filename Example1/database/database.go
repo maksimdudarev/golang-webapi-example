a02042539191dc231645c2c047166d7666e41405
Maksim Dudarev <maksim.dudarev@gmail.com>
2024-09-24 00:35:57 +0300

    add port parsing

--- Example1/database/database.go
+++ Example1/database/database.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/maksimdudarev/golang-webapi-example/models"
 	"gorm.io/driver/postgres"
@@ -18,8 +19,14 @@ type Dbinstance struct {
 var DB Dbinstance
 
 func ConnectDb() {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432",
-		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+	p := os.Getenv("DB_PORT")
+	port, err := strconv.ParseUint(p, 10, 32)
+	if err != nil {
+		log.Fatal("Failed to parse port")
+	}
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d",
+		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), port)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
